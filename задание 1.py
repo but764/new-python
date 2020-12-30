b4fa11d8044eeaca1fd87e07f3b7211515e152e1
butak <but764@yandex.ru>
2020-12-30 20:46:34 +0400

    дз

--- задание 1.py
+++ задание 1.py
@@ -1,10 +1,6 @@
-a = 50
-b = 40
-print("a и b", a, b)
-str1 = input("Введите первую строку")
-num1 = int(input("Введите первое число"))
-str2 = input("Введите вторую строку")
-num2 = int(input("Введите второе число"))
-print("введеные значения - %s %d %s %d" % (str1, num1, str2, num2) )
-
-
+list = [12, 'plant', True, 75.2, None, [1, 2], {22, 4}, bytearray]
+def my_type(el):
+    for el in range(len(list)):
+        print(type(list[el]))
+    return
+my_type(list)
